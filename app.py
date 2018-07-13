949117ed227013ac23b96fa33b9c6d39b60db46f
greysome <wayyan.win@gmail.com>
2018-07-13 11:10:51 +0800

    stuff

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-from flask import Flask, render_template, request
-import twitter
 import re
+from flask import Flask, render_template, request
+from random import randint
+import twitter
 from secret import *
 
 app = Flask(__name__)
 api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token_key,
-                  access_token_secret=access_token_secret)
+                  access_token_secret=access_token_secret,
+                  tweet_mode='extended')
 
 def get_user_posts(username):
     global api
-    posts = api.GetUserTimeline(screen_name=username)
-    posts = [p.text for p in posts]
+    posts = api.GetUserTimeline(screen_name=username, count=200)
+    posts = [p.full_text for p in posts]
     return posts
 
 def extract_url(string):
     return re.search(r'(https?://[^\s]+)', string)
 
 def get_fakeness_index(url):
-    return 1
+    return randint(-10, 11)
 
 @app.route('/')
 def mainpage():
@@ -39,7 +41,12 @@ def posts():
                     posts_with_urls.append((p, url))
 
             rows = [(get_fakeness_index(url), p) for p, url in posts_with_urls]
-            return render_template('posts.html', rows=rows)
+            print(rows[1])
+            return render_template('posts.html', rows=rows, username=username)
 
         except twitter.error.TwitterError:
             return render_template('error.html', message='Username not found.')
+
+@app.route('/stats', methods=['GET', 'POST'])
+def stats():
+    pass
